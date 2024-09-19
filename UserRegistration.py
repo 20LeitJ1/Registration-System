71b1d471386c1eb505b87ed8005cb6b120f5e8e8
Joao Leitao Parente <20LeitJ1@student.allhallows.net>
2024-09-19 11:51:30 +0100

    Added all empty functions

--- UserRegistration.py
+++ UserRegistration.py
@@ -1,4 +1,16 @@
 # Sub Programs
+def CreateUserName():
+    pass
+def CreatePassword():
+    pass
+def AddEmail():
+    pass
+def AddPhoneNumber():
+    pass
+def AddDateOfBirth():
+    pass
+def AddCreditCard():
+    pass
 
 # Main Code
 authenticated = False
